ebe8f53bd64a949b1f2b82f7e5af8c1ba6b1304c
Marijn Ophorst <marijn@monomelodies.nl>
2017-04-08 15:29:53 +0200

    moved to defer

--- src/Plugin.php
+++ src/Plugin.php
@@ -17,31 +17,25 @@ class Plugin extends Core\Plugin
 
     public function __invoke(string $sql) : string
     {
-        if (preg_match_all('@^CREATE VIEW.*?;$@ms', $sql, $views, PREG_SET_ORDER)) {
+        if (preg_match_all('@^CREATE(\s+MATERIALIZED)?\s+VIEW.*?;$@ms', $sql, $views, PREG_SET_ORDER)) {
             foreach ($views as $view) {
                 $sql = str_replace($view[0], '', $sql);
-                $this->views[] = $view[0];
+                $this->defer($view[0]);
             }
         }
         $stmt = $this->loader->getPdo()->prepare(
             "SELECT TABLE_NAME
                 FROM INFORMATION_SCHEMA.TABLES
-                WHERE (TABLE_CATALOG = ? OR TABLE_SCHEMA = ?) AND TABLE_TYPE = 'VIEW'"
+                WHERE ((TABLE_CATALOG = ? AND TABLE_SCHEMA = 'public') OR TABLE_SCHEMA = ?)
+                    AND TABLE_TYPE = 'VIEW'"
         );
         $stmt->execute([$this->loader->getDatabase(), $this->loader->getDatabase()]);
         while (false !== ($view = $stmt->fetchColumn())) {
             if (!$this->loader->shouldBeIgnored($view)) {
-                $this->loader->addOperation("DROP VIEW IF EXISTS $view;");
+                $this->loader->addOperation("DROP VIEW $view;");
             }
         }
         return $sql;
-    }
-
-    public function __destruct()
-    {
-        foreach ($this->views as $view) {
-            $this->loader->addOperation($view);
-        }
     }
 }
 
